420cca1a01f7b635c9c58d09503ecf03ac00ce7a
hoangvu@inspiron <vuhoang1609@gmail.com>
2020-10-06 19:28:54 +0100

    Updates

---
+++ precache-manifest.0337a4124e1c314abbfe3e7c916d02a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3917381971d152077d36dfeaaa2bee01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d89690e2326ab4d5462c",
+    "url": "/static/css/2.c564a22d.chunk.css"
+  },
+  {
+    "revision": "7aaed74af840d177f45f",
+    "url": "/static/css/main.0cb2ce4c.chunk.css"
+  },
+  {
+    "revision": "d89690e2326ab4d5462c",
+    "url": "/static/js/2.3c7db986.chunk.js"
+  },
+  {
+    "revision": "63a44dcd0ffba725ac4bd29695342440",
+    "url": "/static/js/2.3c7db986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aaed74af840d177f45f",
+    "url": "/static/js/main.b399c6c6.chunk.js"
+  },
+  {
+    "revision": "8dbc313d1ee4a46aa244",
+    "url": "/static/js/runtime-main.1c90e072.js"
+  }
+]);
